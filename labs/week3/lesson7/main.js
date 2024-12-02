39e0cd404c12997d7aabf424bfe9da36b0e807a5
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-12-02 21:19:28 +0700

    Lesson 7

---
+++ labs/week3/lesson7/main.js
@@ -0,0 +1,38 @@
+// Create Vue app
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inventory: 0,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
+                },
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+            cart: 0,
+            inStock: false,
+        };
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            this.cart -= 1;
+        },
+        updateImage(vairiantImage) {
+            this.image = vairiantImage;
+        },
+    },
+});
